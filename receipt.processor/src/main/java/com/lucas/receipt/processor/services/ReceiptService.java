7822b95ea737b1b58b0078901d689a972a05b798
wysiatko <lwysiatko@gmail.com>
2023-07-11 23:13:32 -0500

    Implement the bulk of the business logic sans helper methods

--- receipt.processor/src/main/java/com/lucas/receipt/processor/services/ReceiptService.java
+++ receipt.processor/src/main/java/com/lucas/receipt/processor/services/ReceiptService.java
@@ -1,5 +1,6 @@
 package com.lucas.receipt.processor.services;
 
+import com.lucas.receipt.processor.models.requests.Item;
 import com.lucas.receipt.processor.models.requests.ReceiptRequest;
 import com.lucas.receipt.processor.models.responses.ImmutableReceiptPointsResponse;
 import com.lucas.receipt.processor.models.responses.ImmutableReceiptResponse;
@@ -23,7 +24,7 @@ public class ReceiptService {
     public ReceiptResponse processReceipt(ReceiptRequest request) {
 
         ReceiptDataObject receipt = new ReceiptDataObject();
-        receipt.setPoints(2);
+        receipt.setPoints(calculatePoints(request));
 
         receipt = receiptRepository.saveAndFlush(receipt);
         return ImmutableReceiptResponse.builder().id(receipt.getId()).build();
@@ -33,5 +34,64 @@ public class ReceiptService {
 
         ReceiptDataObject receipt = receiptRepository.getById(id);
         return ImmutableReceiptPointsResponse.builder().points(receipt.getPoints()).build();
+    }
+
+    private static long calculatePoints(ReceiptRequest receipt) {
+
+        long totalPoints = 0;
+        totalPoints += alphaNumericCharactersInString(receipt.getRetailer());
+
+        if (isRoundDollarAmount(receipt.getTotal())) {
+            totalPoints += 50;
+        }
+
+        if (isMultipleOf25Cents(receipt.getTotal())) {
+            totalPoints += 25;
+        }
+
+        totalPoints += ((receipt.getItems().size() / 2) * 5L);
+
+        for (Item item : receipt.getItems()) {
+
+            // check if trimmed length of the item description is a multiple of 3
+            if (item.getShortDescription().strip().length() % 3 == 0) {
+                totalPoints += (Math.ceil(item.getPrice() * 0.2));
+            }
+        }
+
+        if (isDateOdd(receipt.getPurchaseDate())) {
+            totalPoints += 6;
+        }
+
+        if (isTimeBetween2pmAnd4pm(receipt.getPurchaseTime())) {
+            totalPoints += 10;
+        }
+
+        return totalPoints;
+    }
+
+    // TODO
+    private static long alphaNumericCharactersInString(String string) {
+        return 0;
+    }
+
+    // TODO
+    private static boolean isRoundDollarAmount(double cost) {
+        return false;
+    }
+
+    // TODO
+    private static boolean isMultipleOf25Cents(double cost) {
+        return false;
+    }
+
+    // TODO
+    private static boolean isDateOdd(String date) {
+        return false;
+    }
+
+    // TODO
+    private static boolean isTimeBetween2pmAnd4pm(String time) {
+        return false;
     }
 }
